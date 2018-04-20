a17395f43e6344a03830262384feece1f4f68a38
Tim Cheung <tim@cheung.io>
2018-04-20 15:09:27 +0200

    switch from ava to jest

--- test/index.test.js
+++ test/index.test.js
@@ -1,27 +1,26 @@
-import test from 'ava';
 import reduceReducers from '../src';
 
-test('combines multiple reducers into a single reducer', t => {
+test('combines multiple reducers into a single reducer', () => {
   const reducer = reduceReducers(
     (prev, curr) => ({ ...prev, A: prev.A + curr }),
     (prev, curr) => ({ ...prev, B: prev.B * curr })
   );
 
-  t.deepEqual(reducer({ A: 1, B: 2 }, 3), { A: 4, B: 6 });
-  t.deepEqual(reducer({ A: 5, B: 8 }, 13), { A: 18, B: 104 });
+  expect(reducer({ A: 1, B: 2 }, 3)).toEqual({ A: 4, B: 6 });
+  expect(reducer({ A: 5, B: 8 }, 13)).toEqual({ A: 18, B: 104 });
 });
 
-test('chains multiple reducers into a single reducer', t => {
+test('chains multiple reducers into a single reducer', () => {
   const addReducer = (prev, curr) => ({ ...prev, A: prev.A + curr });
   const multReducer = (prev, curr) => ({ ...prev, A: prev.A * curr });
   const reducerAddMult = reduceReducers(addReducer, multReducer);
   const reducerMultAdd = reduceReducers(multReducer, addReducer);
 
-  t.deepEqual(reducerAddMult({ A: 1, B: 2 }, 3), { A: 12, B: 2 });
-  t.deepEqual(reducerMultAdd({ A: 1, B: 2 }, 3), { A: 6, B: 2 });
+  expect(reducerAddMult({ A: 1, B: 2 }, 3)).toEqual({ A: 12, B: 2 });
+  expect(reducerMultAdd({ A: 1, B: 2 }, 3)).toEqual({ A: 6, B: 2 });
 });
 
-test('supports additional arguments', t => {
+test('supports additional arguments', () => {
   const addReducer = (prev, curr, scale) => ({
     ...prev,
     A: (prev.A + curr) * scale
@@ -32,5 +31,5 @@ test('supports additional arguments', t => {
   });
   const reducerAddMult = reduceReducers(addReducer, multReducer);
 
-  t.deepEqual(reducerAddMult({ A: 1, B: 2 }, 3, 2), { A: 48, B: 2 });
+  expect(reducerAddMult({ A: 1, B: 2 }, 3, 2)).toEqual({ A: 48, B: 2 });
 });
