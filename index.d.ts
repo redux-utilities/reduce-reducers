3883cdfc5cc84196973a03f4f85c60d44b2435b7
Tim Cheung <tim@cheung.io>
2019-05-05 12:10:01 +0100

    fix type definition export

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,10 @@ type Action = {
 
 type Reducer<S> = (state: S, action: Action) => S;
 
-function reduceReducers<S>(initialState: S | null, ...reducers: Reducer<S>[]): Reducer<S>;
-function reduceReducers<S>(...reducers: Reducer<S>[]): Reducer<S>;
-
-export default reduceReducers;
+export default function reduceReducers<S>(
+  initialState: S | null,
+  ...reducers: Reducer<S>[]
+): Reducer<S>;
+export default function reduceReducers<S>(
+  ...reducers: Reducer<S>[]
+): Reducer<S>;
