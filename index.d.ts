1276e3ce7a8734c7832c82dd0c815a8bdbc0bb6a
Reedik Tuuling <reedik.tuuling@gmail.com>
2019-05-05 03:15:38 +0300

    Update index.d.ts (#31)
    Fix for #30 . Looks like the overloads for #29 were named correctly but the export was left incorrectly singular.

--- index.d.ts
+++ index.d.ts
@@ -7,4 +7,4 @@ type Reducer<S> = (state: S, action: Action) => S;
 function reduceReducers<S>(initialState: S | null, ...reducers: Reducer<S>[]): Reducer<S>;
 function reduceReducers<S>(...reducers: Reducer<S>[]): Reducer<S>;
 
-export default reduceReducer;
+export default reduceReducers;
