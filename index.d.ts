294aaca92e02baf0044415547e69d15af3e7a1ff
Thammi <thammi@chaossource.net>
2020-02-20 14:40:50 +0100

    Improve types to support reducers with and without default handling

--- index.d.ts
+++ index.d.ts
@@ -2,12 +2,21 @@ type Action = {
   type: string;
 };
 
-type Reducer<S> = (state: S, action: Action) => S;
+type Reducer<S> = (state: S | undefined, action: Action) => S;
+type PreloadedReducer<S> = (state: S, action: Action) => S;
 
 export default function reduceReducers<S>(
-  initialState: S | null,
-  ...reducers: Reducer<S>[]
+  initialState: S,
+  ...reducers: PreloadedReducer<S>[]
 ): Reducer<S>;
 export default function reduceReducers<S>(
-  ...reducers: Reducer<S>[]
+  initialReducer: Reducer<S>,
+  ...reducers: PreloadedReducer<S>[]
 ): Reducer<S>;
+export default function reduceReducers<S>(
+  initialState: S | null,
+  ...reducers: PreloadedReducer<S>[]
+): PreloadedReducer<S>;
+export default function reduceReducers<S>(
+  ...reducers: PreloadedReducer<S>[]
+): PreloadedReducer<S>;
